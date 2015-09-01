0e4acd4955b67125f5fb0342d432f5c840866de2
Sean M. Montgomery <produceconsumerobot@gmail.com>
2015-09-01 17:33:07 -0400

    Made some alterations to clean up code.

--- src/LidarLite.cpp
+++ src/LidarLite.cpp
@@ -47,7 +47,6 @@ int LidarLite::getStatus() {
 //--------------------------------------------------------------	
 int LidarLite::readDistance() {
 	int hiVal, loVal, i=0;
-	unsigned char val;
 	
 	unsigned char nackack = 100; // Setup variable to hold ACK/NACK resopnses
 	
@@ -56,14 +55,16 @@ int LidarLite::readDistance() {
 		nackack = wiringPiI2CWriteReg8(_fd, REG_MEASURE, VAL_MEASURE);
 		usleep(1000);
 		//delay(1);
-		if (++i > MAX_TRIES) return -1; // after many tries return an error
+		if (++i > MAX_TRIES) return ERROR_READ; // after many tries return an error
 	}
 	
 	// Get the low byte, return -1 if error occurred
-	if (readByte(_fd, REG_LO_DISTANCE, false, &val) == ERROR_READ) return -1;
-	loVal = val;
-	if (readByte(_fd, REG_HI_DISTANCE, true, &val) == ERROR_READ) return -1;
-	hiVal = val;
+	loVal = readByte(_fd, REG_LO_DISTANCE, false);
+	if (loVal == ERROR_READ) return ERROR_READ;
+	
+	// Get the high byte, return -1 if error occurred
+	hiVal = readByte(_fd, REG_HI_DISTANCE, false);
+	if (hiVal == ERROR_READ) return ERROR_READ;
 	
 	return ( (hiVal << 8) + loVal);
 	//return lidar_read(_fd);
@@ -71,43 +72,39 @@ int LidarLite::readDistance() {
 
 //--------------------------------------------------------------	
 int LidarLite::getHardwareVersion() {
-	unsigned char val;
-	
-	// return zero if we got an error reading
-	if (readByte(_fd, REG_VERSION, false, &val) == ERROR_READ) return -1;
-	
-	// otherwise return the value
-	return (int) val;
-	
-	//return lidar_version(_fd);
+	return readByte(_fd, REG_VERSION, false);
 }
 
 //--------------------------------------------------------------	
 std::string LidarLite::getStatusString(int status) {
 	std::stringstream out;
 	
-	unsigned char stat = (unsigned char) status;
-	
-	// Print the hex status byte
-	out << "STATUS BYTE: 0x";
-	// add a zero if necessary to maintain correct formatting
-	if ((int) stat < 10) out << "0";
-	out << (int) stat;
+	if (status == -1) {
+		out << "STATUS: -1 error";
+	} else {
+		unsigned char stat = (unsigned char) status;
+		
+		// Print the hex status byte
+		out << "STATUS BYTE: 0x";
+		// add a zero if necessary to maintain correct formatting
+		if ((int) stat < 10) out << "0";
+		out << (int) stat;
 
-	if (stat & STATUS_BUSY) out << " busy";              
-	if (stat & STATUS_REFERENCE_OVERFLOW) out << " reference overflow";            
-	if (stat & STATUS_SIGNAL_OVERFLOW) out << " signal overflow";            
-	if (stat & STATUS_PIN) out << " mode select pin";                 
-	if (stat & STATUS_SECOND_PEAK) out << " second peak";         
-	if (stat & STATUS_TIMESTAMP) out << " active between pairs";                
-	if (stat & STATUS_SIGNAL_INVALID) out << " no signal";             
-	if (stat & STATUS_EYE_SAFETY_ON) out << " eye safety";  	
-	
-	return out.str();
+		if (stat & STATUS_BUSY) out << " busy";              
+		if (stat & STATUS_REFERENCE_OVERFLOW) out << " reference overflow";            
+		if (stat & STATUS_SIGNAL_OVERFLOW) out << " signal overflow";            
+		if (stat & STATUS_PIN) out << " mode select pin";                 
+		if (stat & STATUS_SECOND_PEAK) out << " second peak";         
+		if (stat & STATUS_TIMESTAMP) out << " active between pairs";                
+		if (stat & STATUS_SIGNAL_INVALID) out << " no signal";             
+		if (stat & STATUS_EYE_SAFETY_ON) out << " eye safety";  	
+		
+		return out.str();
+	}
 }     
    
 //--------------------------------------------------------------	
-int LidarLite::readByte(int fd, int reg,  bool allowZero, unsigned char *val) {
+int LidarLite::readByte(int fd, int reg,  bool allowZero) {
 	int i=0, tempVal;
 	
 	usleep(1000);
@@ -120,12 +117,10 @@ int LidarLite::readByte(int fd, int reg,  bool allowZero, unsigned char *val) {
 		if (tempVal == ERROR_READ || (tempVal==0 && !allowZero) ) {
 			usleep(1000);
 			//delay (1) ;		// ms
-			*val = tempVal;
 			if (++i > 100) return ERROR_READ; // after many tries return an error
 		} else {
 			// success!
-			*val = tempVal;
-			return 0;
+			return tempVal;
 		}
 	}
 }
